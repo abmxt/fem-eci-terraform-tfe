20845a917e7318397388c541c48a980e0017fa7c
Alberto Bautista <alberto.bautista@lambdamotive.com>
2025-04-27 21:57:03 +0200

    feat: setup vcs automation

--- variables.tf
+++ variables.tf
@@ -1,4 +1,14 @@
 variable "organization_name" {
-    default = "abautista"
-    type = string
+  default = "abautista"
+  type    = string
+}
+
+variable "github_app_installation_id" {
+  default = 65402986
+  type    = number
+}
+
+variable "github_organization_name" {
+  default = "abmxt"
+  type    = string
 }
