2cdbfbf6c4bd77fa3b63bba8f42a02dda065b31f
Alberto Bautista <alberto.bautista@lambdamotive.com>
2025-04-29 22:00:08 +0200

    feat: setup

--- locals.tf
+++ locals.tf
@@ -7,10 +7,17 @@ locals {
 
   workspace = {
     "fem-eci-tfe" = {
-      description    = "Example description of workspace"
-      execution_mode = "remote"
-      project_id     = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "abmxt/fem-eci-terraform-tfe"
+      description         = "Example description of workspace"
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
+    }
+
+    "fem-eci-github" = {
+      description         = "Example description for github resources"
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe-github"
     }
   }
 }
